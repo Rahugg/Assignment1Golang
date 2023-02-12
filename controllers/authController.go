7a7a957317f6e57ce9199023c186aaff440bd080
“Rahugg” <rahugg07@gmail.com>
2023-02-12 20:39:33 +0600

    added a JWT token

--- controllers/authController.go
+++ controllers/authController.go
@@ -1,19 +1,32 @@
 package controllers
 
 import (
+	"strconv"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
 	"golang.org/x/crypto/bcrypt"
 
-	"assignment1GO/models"
 	"assignment1GO/database"
+	"assignment1GO/models"
 )
+
+/*
+JWT is created with a secret key and that secret key is private to you
+which means you will never reveal that to the public or inject inside the JWT token.
+When you receive a JWT from the client, you can verify that JWT with this that secret key stored on the server.
+*/
+const secretKey = "secret"
 
 func Register(c *fiber.Ctx) error {
 	var data map[string]string
+
 	err := c.BodyParser(&data)
 	if err != nil {
 		return err
 	}
+
 	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 14)
 	user := models.User{
 		Name:     data["name"],
@@ -24,4 +37,58 @@ func Register(c *fiber.Ctx) error {
 	database.DB.Create(&user)
 
 	return c.JSON(user)
+}
+
+func Login(c *fiber.Ctx) error {
+	var data map[string]string
+
+	err := c.BodyParser(&data)
+
+	if err != nil {
+		return err
+	}
+
+	var user models.User
+
+	database.DB.Where("email=?", data["email"]).First(&user)
+
+	if user.ID == 0 {
+		c.Status(fiber.StatusNotFound)
+		return c.JSON(fiber.Map{
+			"message": "user not found",
+		})
+	}
+
+	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil {
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": "incorrect password",
+		})
+	}
+
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Issuer:    strconv.Itoa(int(user.ID)),
+		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(), //1 day
+	})
+	token, err := claims.SignedString([]byte(secretKey))
+
+	if err != nil {
+		c.Status(fiber.StatusInternalServerError)
+		return c.JSON(fiber.Map{
+			"message": "could not login",
+		})
+	}
+
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    token,
+		Expires:  time.Now().Add(time.Hour * 24),
+		HTTPOnly: true,
+	}
+
+	c.Cookie(&cookie)
+
+	return c.JSON(fiber.Map{
+		"message": "success",
+	})
 }
