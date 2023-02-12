26ffff39998716b2dc586351b257caa1a9303bdb
“Rahugg” <rahugg07@gmail.com>
2023-02-12 20:12:17 +0600

    installed bcrypt to hash passwords

--- controllers/authController.go
+++ controllers/authController.go
@@ -2,8 +2,23 @@ package controllers
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
+
+	"assignment1GO/models"
 )
 
-func Hello(c *fiber.Ctx) error {
-	return c.SendString("Hello, World 👋!")
+func Register(c *fiber.Ctx) error {
+	var data map[string]string
+	err := c.BodyParser(&data)
+	if err != nil {
+		return err
+	}
+	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 14)
+	user := models.User{
+		Name:     data["name"],
+		Email:    data["email"],
+		Password: password,
+	}
+
+	return c.JSON(user)
 }
--- database/connection.go
+++ database/connection.go
@@ -3,11 +3,14 @@ package database
 import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+
+	"assignment1GO/models"
 )
 
 func Connect() {
-	_, err := gorm.Open(postgres.Open("postgres://postgres:12345@localhost:5432/postgres"), &gorm.Config{})
+	connection, err := gorm.Open(postgres.Open("postgres://postgres:12345@localhost:5432/postgres"), &gorm.Config{})
 	if err != nil {
 		panic("Could not connect to the db")
 	}
+	connection.AutoMigrate(&models.User{})
 }
