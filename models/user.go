54ed3bc98e37835a3333d17bd7893632dab3bc6e
“Rahugg” <rahugg07@gmail.com>
2023-02-12 21:06:06 +0600

    Created a user function

--- models/user.go
+++ models/user.go
@@ -1,7 +1,8 @@
 package models
-type User struct{
-	ID uint
-	Name string
-	Email string `gorm:"unique"`
-	Password []byte
+
+type User struct {
+	ID       uint   `json:"id"`
+	Name     string `json:"name"`
+	Email    string ` json:"email" gorm:"unique"`
+	Password []byte `json:"-"`
 }
